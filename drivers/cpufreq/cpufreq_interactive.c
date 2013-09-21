309c0a5273019b22d87f791d6394975fad6a70b4
n3ocort3x <peter.hofinger@gmail.com>
2013-09-22 01:42:19 +0200

    governor updates, defconfig update

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -69,7 +69,7 @@ static struct mutex gov_lock;
 static u64 hispeed_freq;
 
 /* Go to hi speed when CPU load at or above this value. */
-#define DEFAULT_GO_HISPEED_LOAD 85
+#define DEFAULT_GO_HISPEED_LOAD 65
 static unsigned long go_hispeed_load;
 
 /*
